12f66eb0596f53944b5f4b1e36d8e780920ac765
msmicha200 <msmicha200@gmail.com>
2021-01-17 00:29:05 +0200

    crated jwt-auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Validator;
+
+class AuthController extends Controller
+{
+    //
+
+
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login(Request $request) {
+        if (!$token = auth()->attempt($request->all())) {
+            return response()->json([
+                'error' => 'Unauthorized'
+            ], 401);
+        }
+
+        return $this->createNewToken($token);
+    }
+
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            // data to validate
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create(array_merge(
+            $validator->validate(),
+            ['password' => bcrypt($request->password)]
+        ));
+
+        return response()->json([
+            'success' => 'true',
+            'user' => $user
+        ], 201);
+    }
+
+    public function logout() {
+        auth()->logout();
+
+        return response()->json([
+            'success' => 'true'
+        ]);
+    }
+
+    public function refresh() {
+        return $this->createNewToken(auth()->refresh());
+    }
+
+    protected function createNewToken($token) {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ]);
+    }
+}
